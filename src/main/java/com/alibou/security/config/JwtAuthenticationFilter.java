fcccd6ab76ae54baeef5a511894c4b426965079b
sbremer05 <utw5es@virginia.edu>
2025-04-10 00:32:42 -0400

    AuthenticationFilter started

---
+++ src/main/java/com/alibou/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.alibou.security.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import lombok.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
